8293137c84ff7d5b47fe0e748e416fce88264393
vishalrk1 <karangalevr@gmail.com>
2024-05-13 01:04:20 +0530

    footer submit feedback functionality added

--- app/api/feedbacks/route.ts
+++ app/api/feedbacks/route.ts
@@ -1,11 +1,16 @@
 import prismadb from "@/lib/prismadb";
 import { apiErrorResponse } from "@/utils/utils";
+import { Prisma } from "@prisma/client";
 import { NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req: NextRequest, res: NextApiResponse) {
   try {
     const feedbacks = await prismadb.userFeedbacks.findMany({
+      where: {
+        isFeatured: true,
+      },
       include: {
         user: {
           include: {
@@ -19,6 +24,40 @@ export async function GET(req: NextRequest, res: NextApiResponse) {
       { status: 200 }
     );
   } catch (error) {
+    return apiErrorResponse();
+  }
+}
+
+export async function POST(req: NextRequest, res: NextApiResponse) {
+  const userId = req.nextUrl.searchParams.get("userId");
+  const message = req.nextUrl.searchParams.get("message");
+
+  if (!userId || !message) {
+    return new NextResponse("Missing userId or message", { status: 400 });
+  }
+
+  try {
+    const userFeedback = await prismadb.userFeedbacks
+      .create({
+        data: {
+          message: message,
+          userId: parseInt(userId),
+          id: uuidv4(),
+          createdAt: new Date(),
+          updateddAt: new Date(),
+        },
+      })
+      .catch((e: Prisma.PrismaClientKnownRequestError) => {
+        if (e.code === "P2002") {
+          throw new Error("Feedback already exists");
+        }
+      });
+
+    return NextResponse.json(
+      { message: "feedback created successfully", userFeedback },
+      { status: 200 }
+    );
+  } catch (error: any) {
     return apiErrorResponse();
   }
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,64 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import logo from "../public/assets/Logo Gold.png";
 import Image from "next/image";
-import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { Separator } from "./ui/separator";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store/store";
+import { showErrorToast, showSucessToast } from "@/utils/toasts";
+import axios from "axios";
 
 const Footer = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [message, setMessage] = useState("");
+  const footerNavLinks = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "/about" },
+    { label: "Contact", href: "/contact" },
+  ];
+
+  const handelFeedbackSubmit = async () => {
+    try {
+      const req = await axios.post(
+        `${process.env.NEXT_PUBLIC_ENDPOINT_URL}/feedbacks`,
+        {},
+        {
+          params: {
+            message: message,
+            userId: user?.id,
+          },
+        }
+      );
+      console.log(req.status);
+      if (req.status === 200) {
+        setMessage("");
+        showSucessToast("Feedback submitted");
+      }
+    } catch (error) {
+      setMessage("");
+      showErrorToast("you already have submitted feedback");
+    }
+  };
+
   return (
     <footer className="bg-slate-900 p-4 flex flex-col gap-2">
+      <section className="flex flex-wrap gap-4 md:gap-8 justify-center items-center px-4 py-6">
+        {footerNavLinks.map((item, index) => (
+          <Link
+            key={index}
+            href={item.href}
+            className="text-slate-500 hover:text-white font-light text-lg"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </section>
+      <Separator className="bg-slate-500 mb-6" />
       <section className="flex justify-between items-center">
-        <div className="flex flex-col gap-10 p-6 w-1/2">
+        <div className="flex md:flex-col items-center md:items-start gap-4 p-6 md:w-1/2">
           <Image
             alt="illusion-logo"
             src={logo}
@@ -21,24 +70,25 @@ const Footer = () => {
             Discover our exquisite collection of handcrafted jewelry.
           </p>
         </div>
-        <div className="flex flex-col items-start justify-center gap-2 w-1/2">
+        <div className="hidden md:flex flex-col items-start justify-center gap-2 w-1/2">
           <p className="text-white font-semibold">{`Share Your Feedback`}</p>
-          <Textarea className="bg-transparent text-white border-slate-600" />
+          <Textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="let us know how was your experience buying from us"
+            className="bg-transparent text-white border-slate-600"
+          />
           <Button
             variant="outline"
             className="w-full bg-transparent text-white border-slate-600"
+            onClick={() => {
+              !message || !user
+                ? showErrorToast("Login to submit feedback")
+                : handelFeedbackSubmit();
+            }}
           >
             Submit
           </Button>
-        </div>
-      </section>
-      <section className="flex">
-        <div className="bg-white flex-1 flex justify-center items-center">
-          1
-        </div>
-        <div className="flex-1 flex justify-center items-center">2</div>
-        <div className="bg-white flex-1 flex justify-center items-center">
-          3
         </div>
       </section>
     </footer>
@@ -46,6 +96,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// lol
-
