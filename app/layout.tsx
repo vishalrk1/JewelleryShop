57028c29cd017e3ded157f0860453f6affbafaa1
vishalrk1 <karangalevr@gmail.com>
2024-05-26 03:13:57 +0530

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navBar";
 import { ReduxProvider } from "@/provider/redux-provider";
 import { ToasterProvider } from "@/provider/toast-provider";
 import Footer from "@/components/footer";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import Favicon from "./favicon.png";
+
 const Poopins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata: Metadata = {
   title: "Illusion Jwel",
   description: "Discover our latest and greatest jewelry pieces.",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
@@ -25,6 +31,8 @@ export default function RootLayout({
           <ToasterProvider />
           <Navbar />
           {children}
+          <Analytics />
+          <SpeedInsights />
           <Footer />
         </ReduxProvider>
       </body>
